68e7fdb29ed83cfb9c4346dad883c7c411304a1c
solo <solohuckstep@gmail.com>
2023-01-15 19:02:56 -0500

    added virtual schema for friend count

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      // unique 
+      required: true,
+      // trimmed
+    },
+    email: {
+      type: String,
+      required: true,
+      // unique
+      // must match a vaild email adress (look into mongoose's matching validation)
+    },
+    // Array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    // Array of _id values referencing the User model (self-reference)
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',  
+        },
+    ],
+  },
+  { 
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('course', userSchema);
+
+module.exports = User;
