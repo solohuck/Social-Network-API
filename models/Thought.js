71ca7adfbc7a819cab339fd14d57bd42c56e4198
solo <solohuckstep@gmail.com>
2023-01-15 19:05:04 -0500

    added virtual reaction count

--- models/Thought.js
+++ models/Thought.js
@@ -29,6 +29,12 @@ const thoughtSchema = new Schema(
   }
 );
 
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+
 const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
