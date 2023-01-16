4e9ce7b5d7ef57c82555432d0c9de5617270069a
solo <solohuckstep@gmail.com>
2023-01-16 16:53:30 -0500

    added addFriend and removeFriend routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,8 @@ const {
   postUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
 // Routing for /api/users endpoint
@@ -17,6 +19,11 @@ router.route('/:id')
   .get(getSingleUser) // handle GET requests to retrieve a single user by its id
   .put(updateUser) // handle PUT requests to update a user by its id
   .delete(deleteUser); // handle DELETE requests to remove a user by its id
+
+// Routing for /api/users/:userId/friends/:friendId endpoint
+router.route('/:userId/friends/:friendId')
+  .post(addFriend) // handle POST requests to add a new friend to a user's friend list
+  .delete(removeFriend); // handle DELETE requests to remove a friend from a user's friend list
 
 
 module.exports = router;
